166533491570be5391a3f522dcb66dcacafb8662
Anja-dhnd <anais.dehand@gmail.com>
2021-11-25 16:32:45 +0000

    progress of day

--- OOP/play.php
+++ OOP/play.php
@@ -3,9 +3,9 @@
 class Ship
 {
     public $name;
-    public $weaponPower;
-    public $jediFactor;
-    public $strength;
+    public $weaponPower = 0;
+    public $jediFactor = 0;
+    public $strength = 0;
 
     public function sayHello()
     {
@@ -16,13 +16,62 @@ class Ship
     {
         return $this->name;
     }
+
+    public function getNameAndSpecs($useShortFormat)
+
+    {
+        if($useShortFormat){
+            return sprintf('%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength);
+
+        } else {
+
+            return sprintf('%s: w:%s, j:%s, s:%s',
+            $this->name,
+            $this->weaponPower,
+            $this->jediFactor,
+            $this->strength);
+        }
+    }
+    public function strengthComparator($givenShip)
+    {
+        return $givenShip->strength > $this->strength;
+    }
+}
+
+function printShipSummary($someShip){
+
+   echo 'Ship name: ' .$someShip->name;
+    echo '<hr />';
+    $someShip->sayHello();
+    echo '<hr />';
+    echo $someShip->getName();
+    echo '<hr />';
+    var_dump($someShip->strength);
+    echo '<hr />';
+    echo $someShip->getNameAndSpecs(true);
 }
 
 $myShip = new Ship();
 $myShip->name = 'Jedi Starship';
+$myShip->strength = 20;
 
-echo 'Ship name: ' .$myShip->name;
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->strength = 50;
+$otherShip->weaponPower = 10;
+
+printShipSummary($myShip);
 echo '<hr />';
-$myShip->sayHello();
-echo '<hr />';
-echo $myShip->getName();
+printShipSummary($otherShip);
+
+// strength comparison
+echo'<hr />';
+if ($myShip->strengthComparator($otherShip)) {
+    echo $otherShip->name . 'has more strength ';
+} else {
+    echo $myShip->name .' has more strength';
+}
