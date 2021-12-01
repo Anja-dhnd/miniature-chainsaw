a3be949c38a1945bbb4b6b388e101d5d7a681757
Anja-dhnd <anais.dehand@gmail.com>
2021-12-01 16:18:16 +0000

    require added

--- Classroom/Encounter.php
+++ Classroom/Encounter.php
@@ -2,11 +2,11 @@
 
 class Encounter
 {
-    function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo): float|int
+    public function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo): float|int
     {
         return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
     }
-    function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+    public function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
     {
         if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
             trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
